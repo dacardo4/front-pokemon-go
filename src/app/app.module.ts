9ba505de908b78ac05b378cf4681f708677a6f9b
David Trompa <dacardo4@gmail.com>
2022-07-11 18:02:33 -0500

    modulo de pokemon para registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,23 @@ import { PokemonAvailableOnGameComponent } from './components/pokemon-available-
 import { PokemonRegisteredOnGameComponent } from './components/pokemon-registered-on-game/pokemon-registered-on-game.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PokemonDataComponent } from './components/shared/pokeapi/pokemon-data/pokemon-data.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
     PokemonAvailableOnGameComponent,
-    PokemonRegisteredOnGameComponent
+    PokemonRegisteredOnGameComponent,
+    PokemonDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/shared/pokeapi/pokemon-data/pokemon-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokeapi-pokemonData',
+  templateUrl: './pokemon-data.component.html',
+  styleUrls: ['./pokemon-data.component.css']
+})
+export class PokemonDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
