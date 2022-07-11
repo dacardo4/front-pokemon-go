9bc779402db4146d31b2dbdd3b162215e9741295
David Trompa <dacardo4@gmail.com>
2022-07-11 11:34:12 -0500

    navegacion entre componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonAvailableOnGameComponent } from './components/pokemon-available-on-game/pokemon-available-on-game.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonRegisteredOnGameComponent } from './components/pokemon-registered-on-game/pokemon-registered-on-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pokemonList', component: PokemonListComponent },
+  { path: 'availableOnGame', component: PokemonAvailableOnGameComponent },
+  { path: 'registered', component: PokemonRegisteredOnGameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
