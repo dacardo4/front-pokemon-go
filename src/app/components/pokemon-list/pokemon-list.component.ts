de9f46ec32f862e9cd2d77db0bc096f37cf37288
David Trompa <dacardo4@gmail.com>
2022-07-11 16:47:27 -0500

    consulta de pokemon desde pokemonList

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
+import { PokemonService } from 'src/app/services/pokeapi/pokemon.service';
 import { pokemonInList, QUERY_ALL_POKEMON_LIST } from './pokemon-list.querys';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -10,18 +12,32 @@ import { pokemonInList, QUERY_ALL_POKEMON_LIST } from './pokemon-list.querys';
 export class PokemonListComponent implements OnInit {
 
   constructor(
-    private _apollo: Apollo
+    private _apollo: Apollo,
+    private _pokemonService: PokemonService,
+    
   ) { }
 
   pokemonList: pokemonInList[] = [];
 
   ngOnInit(): void {
-    this._apollo.query({
-      query: QUERY_ALL_POKEMON_LIST,
-    }).subscribe((result: any) => {
-      console.log(result);
-      this.pokemonList = result?.data?.pokemonList;
-    });
+    // this._apollo.query({
+    //   query: QUERY_ALL_POKEMON_LIST,
+    // }).subscribe((result: any) => {
+    //   console.log(result);
+    //   this.pokemonList = result?.data?.pokemonList;
+    // });
+    this.getPokemonByID();
+  }
+
+  getPokemonByID(): void {
+    this._pokemonService.getOnePokemonByID(1).pipe(take(1)).subscribe(
+      data => {
+        console.log(data);
+        //this.allCantidades = data;
+      }, error => {
+        console.log('getProductos Error: ',error);
+      }
+    );
   }
 
 }
