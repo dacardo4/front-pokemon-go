e1c04fb02ac47d15182dacfb2bfad09308ec26a8
David Trompa <dacardo4@gmail.com>
2022-07-11 15:53:03 -0500

    ajustes hasura admin

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,14 +1,33 @@
 import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
 
-const uri = 'https://pokemon-go-data.hasura.app/v1/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://pokemon-go-data.hasura.app/v1/graphql';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+
+  const auth = setContext((operation, context) => {
+    return {
+      headers: {
+        "x-hasura-admin-secret": 'tC0T9Esel1uUrksOiCF7xvSWPjnfUsdIuJMcD8NlnxmJuQtDjf4kR7PKQRyNBarp'
+      }
+    };
+  });
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
   return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
-  };
+    link,
+    cache
+  }
 }
 
 @NgModule({
